b23d76ac96e34d70fd243bdf469f98b45698cfe8
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-09-10 11:31:02 +0200

    Fixed manage script.

--- manage.sh
+++ manage.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # Pixelpunk Gamelets #1
-# v0.4.0
-
-export MINTER_CANISTER=$(dfx canister id minter)
+# v0.4.1
 
 function help () {
     echo "Usage:"
     echo "  ./manage.sh <option> [param]"
     echo "  Options:"
-    echo "  deploy <canister> [--ic] - deploy canister"
-    echo "  mint <nft id> <asset url> [--ic] - mint collection"
-    echo "  transfer <nft id> <to principal> [--ic] - transfer NFT to new owner"
-    echo "  redeem <code> - coupon code"
+    echo "    deploy <canister> [--ic] - deploy canister"
+    echo "    mint <nft id> <asset url> [--ic] - mint collection"
+    echo "    transfer <nft id> <to principal> [--ic] - transfer NFT to new owner"
+    exit 0
 }
 
-if [ $# == 0 ]; then
+if [ $# == 0 ] || [ $1 == 'help' ] ; then
     help
     exit 0
 fi
 
+export MINTER_CANISTER=$(dfx canister id minter)
+
 if [ $1 == "deploy" ] && [ $2 == "assets" ]; then
     echo "Installing Asset canister..."
 
-    dfx deploy assets --ic
+    dfx deploy assets $3
     
     exit 0
 fi
@@ -31,15 +31,15 @@ fi
 if [ $1 == "deploy" ] && [ $2 == "minter" ]; then
     echo "Installing Minter canister and initialize collection..."
 
-    dfx deploy minter --argument 'record {icrc7_args = null; icrc37_args =null; icrc3_args =null;}' --mode reinstall --ic
-    dfx canister call minter init --ic
-    dfx canister call minter icrc7_name --query --ic
+    dfx deploy minter --argument 'record {icrc7_args = null; icrc37_args =null; icrc3_args =null;}' --mode reinstall $3
+    dfx canister call minter init $3
+    dfx canister call minter icrc7_name --query $3
     
     exit 0
 fi
 
 if [ $1 == "mint" ]; then
-    echo "Minting NFT $2 @ $3..."
+    echo "Minting NFT id $2 url $3..."
 
     dfx canister call minter icrcX_mint "(
       vec {
@@ -62,9 +62,9 @@ if [ $1 == "mint" ]; then
             created_at_time = null;
         };
       },
-    )" --ic
+    )" $4
 
-    dfx canister call minter icrc7_total_supply --query --ic
+    dfx canister call minter icrc7_total_supply --query $4
     
     exit 0
 fi
@@ -78,16 +78,10 @@ if [ $1 == "transfer" ]; then
         to = record { owner = principal \"$3\"; subaccount = null};
         token_id = $2 : nat;
         memo = null;
-        created_at_time = null;}})"
+        created_at_time = null;}})" $4
 
     dfx canister call minter icrc7_owner_of "(vec {$2})" --query $4
 
-    exit 0
-fi
-
-if [ $1 == "redeem" ]; then
-    # dfx canister --network=ic call fg7gi-vyaaa-aaaal-qadca-cai redeem "(\"$1\")"
-    dfx cycles --ic redeem-faucet-coupon $2
     exit 0
 fi
 
