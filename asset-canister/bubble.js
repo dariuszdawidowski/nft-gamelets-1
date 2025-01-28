3ceb695f1f2b72ed738d84515a4c8bcadafdc07f
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2025-01-28 23:04:11 +0100

    Popup.

--- asset-canister/bubble.js
+++ asset-canister/bubble.js
@@ -1,6 +1,6 @@
 /**
  * Info bubble
- * v 0.1.0
+ * v 0.2.0
  */
 
 class Bubble {
@@ -66,6 +66,7 @@ class Bubble {
             if (remaining <= 0) {
                 clearInterval(this.interval);
                 this.interval = null;
+                this.element.style.display = 'none';
             } else {
                 this.element.innerHTML = msg.replace('{{sec}}', remaining);
             }
